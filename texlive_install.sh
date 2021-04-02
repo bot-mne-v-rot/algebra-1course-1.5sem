a8272e4304b970ba7216d61fe384730d887eb1a3
Andrei Kozyrev <kozyrev.andreiii2016@gmail.com>
2021-04-03 00:26:40 +0300

    ggwp

--- texlive_install.sh
+++ texlive_install.sh
@@ -6,14 +6,14 @@
 # It is intended to work on their VM set up: Ubuntu 12.04 LTS
 # A minimal current TL is installed adding only the packages that are
 # required
-tlmgr option repository ftp://tug.org/historic/systems/texlive/2020/tlnet-final
+sh update-tlmgr-latest.sh -- --upgrade 
 # See if there is a cached version of TL available
 export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
 if ! command -v texlua > /dev/null; then
   # Obtain TeX Live
   wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
   tar -xzf install-tl-unx.tar.gz
-  cd install-tl-21*
+  cd install-tl-20*
 
   # Install a minimal system
   ./install-tl --profile=../texlive/texlive.profile
@@ -21,6 +21,7 @@ if ! command -v texlua > /dev/null; then
   cd ..
 fi
 
+tlmgr option repository ftp://tug.org/historic/systems/texlive/2020/tlnet-final
 # Just including texlua so the cache check above works
 # Needed for any use of texlua even if not testing LuaTeX
 tlmgr install luatex
